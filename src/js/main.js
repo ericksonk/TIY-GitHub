42c0c68251feb1ae7bcdbc0eb11d7e1ca33cfc4e
Kristine Erickson <ericksonk09@gmail.com>
2015-10-05 20:09:01 -0400

    makes tab active on click but is selecting all of tabs instead of one at a time

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ jQuery.ajax('../apis/github/users/octocat.json').then(function(data){
 });
 
 // http://www.jacklmoore.com/notes/jquery-tabs/
-$('ul.tabs').each(function(){ // selects each element inside the ul tag
+$('ul.tabs').on('click', function(){ // selects each element inside the ul tag
   var $active = $(this).find('a'); // `this` refers to containig element (ul.tabs); finds all `a` elements
   $active.addClass('active');
 });
