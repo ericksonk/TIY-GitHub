117f2f3903a1d029f2a12692355c2a897cc0dcc6
Kristine Erickson <ericksonk09@gmail.com>
2015-10-05 23:29:53 -0400

    all tabs displaying repo content, moving on for now

--- src/js/main.js
+++ src/js/main.js
@@ -13,25 +13,26 @@ $('ul.tabs li a').on('click', function(e) {
   $(this).addClass('active');
 
     var viewTab = $(this).attr('href');
-    $('.panel').hide();
-    $('.panel').hide();
-    $('viewTab').show();
-
-    // if ($('#tab2')) {
-    //   $('section#tab2').show();
-    //   $('section#tab3').hide();
-    //   $('section#tab1').hide();
-    // }
-    //
-    // if ($('#tab1 active')) {
-    //   $('section#tab1').show();
-    //   $('section#tab2').hide();
-    //   $('section#tab3').hide();
-    // }
-    //
-    // if ($('#tab3 active')) {
-    //   $('section#tab3').show();
-    //   $('section#tab2').hide();
-    //   $('section#tab1').hide();
-    // }
+      $('#tab1').hide();
+      $('#tab3').hide();
+      $('viewTab').show();
+        e.preventDefault();
 });
+
+// if ($('#tab2' && 'active')) {
+//   $('section#tab2').show();
+//   $('section#tab3').hide();
+//   $('section#tab1').hide();
+// }
+//
+// if ($('#tab1' && 'active')) {
+//   $('section#tab1').show();
+//   $('section#tab2').hide();
+//   $('section#tab3').hide();
+// }
+//
+// if ($('#tab3' && 'active')) {
+//   $('section#tab3').show();
+//   $('section#tab2').hide();
+//   $('section#tab1').hide();
+// }
