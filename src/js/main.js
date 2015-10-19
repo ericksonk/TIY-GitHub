31a958f76da8665564782b74a7b9c0d42ca30f37
Kristine Erickson <ericksonk09@gmail.com>
2015-10-19 17:53:30 -0400

    set up IIFE and module

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,32 @@
+;(function(){ // START IIFE
+  angular.module("tiy-github", [ ])
+  
+  ; // END MODULE
+})(); // END IIFE
+
+
+
 // jQuery.ajax('../apis/github/users/octocat.json').then(function(data){
 //   console.log(arguments);
 // });
 
+// AJAX THINGS
 // source : https://api.jquery.com/jQuery.getJSON/
-$.getJSON("../apis/github/users/ericksonk.json", function (data) {
-  // source: http://www.tutorialspoint.com/jquery/ajax-jquery-getjson.htm
-  $('.fullName').html('<h1>' + data.name); // get the html contents of the 1st matched element
-  $('.userName').html('<p>' + data.login);
-  $('.company').html('<li>' + data.company);
-  $('.location').html('<li>' + data.location);
-  $('.email').html('<a>' + data.email);
-  $('.url').html('<a>' + data.blog);
-  $('.created').html('<li>' + data.created_at);
+// $.getJSON("../apis/github/users/ericksonk.json", function (data) {
+//   // source: http://www.tutorialspoint.com/jquery/ajax-jquery-getjson.htm
+//   $('.fullName').html('<h1>' + data.name); // get the html contents of the 1st matched element
+//   $('.userName').html('<p>' + data.login);
+//   $('.company').html('<li>' + data.company);
+//   $('.location').html('<li>' + data.location);
+//   $('.email').html('<a>' + data.email);
+//   $('.url').html('<a>' + data.blog);
+//   $('.created').html('<li>' + data.created_at);
 
 //Trying to make below function work but moving forward for now...
   // $.each(data, function(key, val) {
   //   $('.' + key).text(val.value);
   // });
-});
+// });
 
 // CLASS NOTES
 
