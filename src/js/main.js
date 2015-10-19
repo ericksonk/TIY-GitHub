aa099dcb41b4933942cd7d04da4451f80dadf56a
Kristine Erickson <ericksonk09@gmail.com>
2015-10-18 20:07:56 -0400

    repository panel active on load

--- src/js/main.js
+++ src/js/main.js
@@ -58,17 +58,17 @@ $.getJSON("../apis/github/users/ericksonk.json", function (data) {
 //   $active.addClass('active');
 // });
 $(document).ready(function(){
-  $('nav.tabs a').on('click', function(e) {
-    var viewTab = $(this).attr('href');
-    $('a').removeClass('active');
-    $('.panel').removeClass('active');
-    $(this).addClass('active');
-    $('#'+viewTab).addClass('active');
+  $('#tab1').hide(); // hides panel 1 on load
+  $('#tab3').hide(); // hides panel 3 on load
 
-      // var viewTab = $(this).attr('href');
-      //   $('#tab1').hide();
-      //   $('#tab3').hide();
-      //   $('viewTab').show();
+  $('nav.tabs a').on('click', function(e) {
+    $('a').removeClass('active');
+    $(this).addClass('active');
+
+      var viewTab = $(this).attr('href');
+        $('#tab1').hide();
+        $('#tab3').hide();
+        viewTab.show();
   });
 });
 
